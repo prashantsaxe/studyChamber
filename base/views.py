b9fbf9e2ed33f6399801f609dc16350ef856a857
Prashant <prashantiiitnag@gmail.com>
2025-03-06 01:40:35 +0530

    db

--- base/views.py
+++ base/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Room
 # Create your views here.
 
 def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
     return render(request,"home.html")
 
 
-def room(request):
+def room(request,pk):
+    room = Room.object.get(id=pk)
+    context = {'room':room}
     return render(request,"room.html")
